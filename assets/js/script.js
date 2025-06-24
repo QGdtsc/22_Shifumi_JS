be552fe307b3c27e97bebe68b32b6f4c18fe89cc
QGdtsc <qgdtsc@gmail.com>
2025-06-24 20:07:32 +0200

    first upload - all files

---
+++ assets/js/script.js
@@ -0,0 +1,184 @@
+let userChoice = ''
+let opponentChoice = ''
+let winner = ''
+let counter_user = 0
+let counter_opponent = 0
+let button_accessibility = true
+
+
+function startGame() {
+    document.getElementById("initial_start").style.display = 'none'
+    document.getElementById("after_start_1").style.display = 'grid'
+    document.getElementById("after_start_2").style.display = 'flex'
+    document.getElementById("message_result").style.display = 'flex'
+    document.getElementById("message_result").innerText = "What's your choice ?"
+}
+
+
+function restartGame() {
+    document.getElementById("initial_start").style.display = 'flex'
+    document.getElementById("after_start_1").style.display = 'none'
+    document.getElementById("after_start_2").style.display = 'none'
+    document.getElementById("message_result").style.display = 'none'
+    counter_user = 0
+    counter_opponent = 0
+    document.getElementById('score_user').innerText = counter_user
+    document.getElementById('score_opponent').innerText = counter_opponent
+    document.getElementById('screen_user').innerHTML = ''
+    document.getElementById('screen_opponent').innerHTML = ''
+    document.getElementById('message_result').innerText = ''
+    colorizeStatus('')
+}
+
+
+function switchAccessibilityButtons() {
+    // console.log(button_accessibility)
+    button_accessibility == true ? disableButtons() : enableButtons()
+}
+function disableButtons() {
+    document.getElementById('buttonUserFist').disabled = true
+    document.getElementById('buttonUserPalm').disabled = true
+    document.getElementById('buttonUserScissors').disabled = true
+    document.getElementById('buttonUserFist').style.backgroundColor = 'oklch(55.6% 0 0)'
+    document.getElementById('buttonUserPalm').style.backgroundColor = 'oklch(55.6% 0 0)'
+    document.getElementById('buttonUserScissors').style.backgroundColor = 'oklch(55.6% 0 0)'
+    button_accessibility = false
+}
+function enableButtons() {
+    document.getElementById('buttonUserFist').disabled = false
+    document.getElementById('buttonUserPalm').disabled = false
+    document.getElementById('buttonUserScissors').disabled = false
+    document.getElementById('buttonUserFist').style.backgroundColor = 'oklch(82.7% 0.119 306.383)'
+    document.getElementById('buttonUserPalm').style.backgroundColor = 'oklch(82.7% 0.119 306.383)'
+    document.getElementById('buttonUserScissors').style.backgroundColor = 'oklch(82.7% 0.119 306.383)'
+    button_accessibility = true
+}
+
+
+function getChoice(choice) {
+    // console.log('CLICK')
+    userChoice = choice
+    switch (userChoice) {
+        case 'fist':
+            html_element = "<button class='btn border border-1 aspect-square rounded-md bg-purple-300 w-[35vw] lg:w-[8vw] flex justify-center items-center'><img class='w-4/5 h-4/5' src='assets/icons/hand-back-fist-regular.svg' alt='image de poing'></button>"
+            break;
+        case 'palm':
+            html_element = "<button class='btn border border-1 aspect-square rounded-md bg-purple-300 w-[35vw] lg:w-[8vw] flex justify-center items-center'><img class='w-4/5 h-4/5' src='assets/icons/hand-regular.svg' alt='image de main'></button>"
+            break;
+        case "scissors":
+            html_element = "<button class='btn border border-1 aspect-square rounded-md bg-purple-300 w-[35vw] lg:w-[8vw] flex justify-center items-center'><img class='w-4/5 h-4/5' src='assets/icons/hand-scissors-regular.svg' alt='image de ciseaux'></button>"
+            break;
+        // default:
+        //     break;
+    }
+    document.getElementById('screen_user').innerText = userChoice
+    document.getElementById('screen_user').innerHTML = html_element
+    opponentChoice, html_element_opponent = getRandomChoiceOpponent();
+    message = comparisonElement(userChoice, opponentChoice)
+    document.getElementById('message_result').innerText = 'Waiting for opponent'
+    document.getElementById('message_result').style.backgroundColor = 'oklch(95.4% 0.038 75.164)'
+    document.getElementById('message_result').style.color = 'oklch(27.9% 0.077 45.635)'
+    document.getElementById('screen_opponent').innerHTML = ''
+    switchAccessibilityButtons()
+    setTimeout(() => {
+        displayOpponentChoiceAndStatus(html_element_opponent, message, counter_user, counter_opponent)
+    }, 500);
+}
+
+
+function displayOpponentChoiceAndStatus(html_element, message, counter_user, counter_opponent) {
+    document.getElementById('screen_opponent').innerHTML = html_element
+    document.getElementById('message_result').innerText = message
+    document.getElementById('score_user').innerText = counter_user
+    document.getElementById('score_opponent').innerText = counter_opponent
+    colorizeStatus(message)
+    switchAccessibilityButtons()
+}
+
+
+function colorizeStatus(status) {
+    switch (status) {
+        case 'VICTORY':
+            document.getElementById('message_result').style.backgroundColor = 'green'
+            document.getElementById('message_result').style.color = 'white'
+            document.getElementById('message_result').style.borderTop = '3px solid oklch(27.9% 0.077 45.635)'
+            break;
+        case 'DEFEAT':
+            document.getElementById('message_result').style.backgroundColor = 'red'
+            document.getElementById('message_result').style.color = 'white'
+            document.getElementById('message_result').style.borderTop = '3px solid oklch(27.9% 0.077 45.635)'
+            break;
+        // case 'EQUALITY':
+        //     document.getElementById('message_result').style.backgroundColor = 'oklch(95.4% 0.038 75.164)'
+        //     document.getElementById('message_result').style.color = 'oklch(27.9% 0.077 45.635)'
+        //     break;
+        default:
+            document.getElementById('message_result').style.backgroundColor = 'oklch(95.4% 0.038 75.164)'
+            document.getElementById('message_result').style.color = 'oklch(27.9% 0.077 45.635)'
+            break;
+    }
+}
+
+
+// function getRandomInt(max) {
+//     return Math.floor(Math.random() * max);
+// }
+getRandomInt = (max) => Math.floor(Math.random() * max)
+function getRandomChoiceOpponent() {
+    let randomInt = getRandomInt(3)
+    // console.log(randomInt)
+    switch (randomInt) {
+        case 0:
+            opponentChoice = "fist"
+            html_element = "<button class='btn border border-1 aspect-square rounded-md bg-red-300 w-[35vw] lg:w-[8vw] flex justify-center items-center'><img class='w-4/5 h-4/5' src='assets/icons/hand-back-fist-regular.svg' alt='image de poing'></button>"
+            break;
+        case 1:
+            opponentChoice = "palm"
+            html_element = "<button class='btn border border-1 aspect-square rounded-md bg-red-300 w-[35vw] lg:w-[8vw] flex justify-center items-center'><img class='w-4/5 h-4/5' src='assets/icons/hand-regular.svg' alt='image de main'></button>"
+            break;
+        case 2:
+            opponentChoice = "scissors"
+            html_element = "<button class='btn border border-1 aspect-square rounded-md bg-red-300 w-[35vw] lg:w-[8vw] flex justify-center items-center'><img class='w-4/5 h-4/5' src='assets/icons/hand-scissors-regular.svg' alt='image de ciseaux'></button>"
+            break;
+        default:
+            break;
+    }
+    // document.getElementById('screen_opponent').innerText = opponentChoice
+    // document.getElementById('screen_opponent').innerHTML = html_element
+    return opponentChoice, html_element
+}
+
+
+function comparisonElement(userChoice, opponentChoice) {
+    // console.log(userChoice)
+    // console.log(opponentChoice)
+    switch (userChoice) {
+        case 'fist':
+            if (opponentChoice == 'fist') { winner = 'equality' }
+            if (opponentChoice == 'palm') { winner = 'opponent' }
+            if (opponentChoice == 'scissors') { winner = 'user' }
+            break;
+        case 'palm':
+            if (opponentChoice == 'fist') { winner = 'user' }
+            if (opponentChoice == 'palm') { winner = 'equality' }
+            if (opponentChoice == 'scissors') { winner = 'opponent' }
+            break;
+        case 'scissors':
+            if (opponentChoice == 'fist') { winner = 'opponent' }
+            if (opponentChoice == 'palm') { winner = 'user' }
+            if (opponentChoice == 'scissors') { winner = 'equality' }
+            break;
+        default:
+            break;
+    }
+    if (winner == 'user') {
+        counter_user += 1
+        message = 'VICTORY'
+    } else if (winner == 'opponent') {
+        counter_opponent += 1
+        message = 'DEFEAT'
+    } else {
+        message = 'EQUALITY'
+    }
+    return message
+}
